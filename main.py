87bee08875fe5333131874ea182edb7b43b12d4c
Tiago Henrique <tiagos1628@gmail.com>
2025-07-24 19:32:26 -0300

    initial comits

---
+++ main.py
@@ -0,0 +1,13 @@
+import pygame
+
+print('Setup Start')
+pygame.init()
+window = pygame.display.set_mode(size=(600, 480)) # aqui faço a criação da janela com tamanho especificado
+print('Setup End')
+while True:
+    #chek all events
+    for event in pygame.event.get(): # comando obtido na doc pygame
+        if event.type == pygame.QUIT:
+            pygame.quit()  # close window
+            quit() #end pygame
+
